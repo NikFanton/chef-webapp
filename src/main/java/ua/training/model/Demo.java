eafa433daa819378741bc82d3d9e64e23faecf3d
Nikita Guchenko <guchenko.n@gmail.com>
2018-04-05 18:26:54 +0300

    refactoring

--- src/main/java/ua/training/model/Demo.java
+++ src/main/java/ua/training/model/Demo.java
@@ -1,4 +1,18 @@
 package ua.training.model;
 
+import ua.training.model.builder.Director;
+import ua.training.model.entity.Salad;
+
+import javax.lang.model.element.NestingKind;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
 public class Demo {
+    public static void main(String[] args) {
+
+
+    }
 }
