183e9bf4c17da8f09023c5c3fc225b70c89992ce
Nikita Guchenko <guchenko.n@gmail.com>
2018-04-05 18:29:44 +0300

    added calories service

--- src/main/java/ua/training/model/service/CaloriesService.java
+++ src/main/java/ua/training/model/service/CaloriesService.java
@@ -2,14 +2,31 @@ package ua.training.model.service;
 
 import ua.training.model.entity.vegetable.Vegetable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CaloriesProcessor {
-    public static double getTotalCalories(List<Vegetable> vegetables) {
-        double totalCalories = 0.0;
+public class CaloriesService {
+    public static int getTotalCalories(List<Vegetable> vegetables) {
+        int totalCalories = 0;
         for (Vegetable v : vegetables) {
-            totalCalories += v.getCalories()/100. * v.getWeight();
+            totalCalories += calcCalories(v);
         }
         return totalCalories;
+    }
+
+    public static List<Vegetable> getVegetablesWithCaloriesInDiapason(List<Vegetable> vegetables, int min, int max) {
+        List<Vegetable> result = new ArrayList<>();
+        int calories = 0;
+        for (Vegetable v : vegetables) {
+            calories = calcCalories(v);
+            if (calories >= min && calories <= max) {
+                result.add(v);
+            }
+        }
+        return vegetables;
+    }
+
+    private static int calcCalories(Vegetable v) {
+        return (int) (v.getCalories()/100. * v.getWeight());
     }
 }
