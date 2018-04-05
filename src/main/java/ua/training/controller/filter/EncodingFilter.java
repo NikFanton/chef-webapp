62d7aa8257c97755fb289c621b32fdc02daf9ad5
Nikita Guchenko <guchenko.n@gmail.com>
2018-04-05 18:26:21 +0300

    created encoding filter

--- src/main/java/ua/training/controller/filter/EncodingFilter.java
+++ src/main/java/ua/training/controller/filter/EncodingFilter.java
@@ -1,4 +1,22 @@
 package ua.training.controller.filter;
 
-public class Filter {
+import javax.servlet.*;
+import java.io.IOException;
+
+public class EncodingFilter implements javax.servlet.Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
 }
